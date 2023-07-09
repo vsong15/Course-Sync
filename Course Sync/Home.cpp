41777c419dd4584993667a9cc033cd72ca416325
Vincent Song <dela.vincent.15@gmail.com>
2023-07-09 01:48:38 -0400

    Add login button and text fields

--- Course Sync/Home.cpp
+++ Course Sync/Home.cpp
@@ -33,6 +33,84 @@ void Home::Display(HWND hWnd)
     SelectObject(hdc, hDescFont);
     DrawTextCenter(hdc, L"Student Enrollment Database Management System", 80, width, height);
 
+    // Calculate the label and text field widths
+    int labelWidth = 150;
+    int textFieldWidth = 200;
+
+    // Calculate the left margin for centering
+    int leftMargin = (width - (labelWidth + textFieldWidth)) / 2;
+
+    // Adjust the left and right margins
+    int newLeftMargin = leftMargin - 40;  // Decrease the left margin
+    int newRightMargin = leftMargin + textFieldWidth + labelWidth + 40;  // Increase the right margin
+
+    // Draw the username label
+    HFONT hLabelFont = CreateFont(20, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET,
+        OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, L"Arial");
+    SelectObject(hdc, hLabelFont);
+    RECT usernameLabelRect;
+    SetRect(&usernameLabelRect, newLeftMargin, 230, newLeftMargin + labelWidth, 260); // Updated position
+    DrawText(hdc, L"Username:", -1, &usernameLabelRect, DT_SINGLELINE | DT_LEFT | DT_VCENTER | DT_NOCLIP | DT_NOPREFIX);
+
+    // Draw the username input field
+    HFONT hInputFont = CreateFont(20, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET,
+        OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, L"Arial");
+    SelectObject(hdc, hInputFont);
+    RECT usernameRect;
+    SetRect(&usernameRect, newLeftMargin + labelWidth, 230, newRightMargin, 260); // Updated position
+    HBRUSH hInputBrush = CreateSolidBrush(RGB(255, 255, 255)); // White background color
+    FillRect(hdc, &usernameRect, hInputBrush);
+    DrawText(hdc, L"", -1, &usernameRect, DT_SINGLELINE | DT_LEFT | DT_VCENTER | DT_EDITCONTROL | DT_NOCLIP | DT_NOPREFIX);
+
+    // Draw the password label
+    RECT passwordLabelRect;
+    SetRect(&passwordLabelRect, newLeftMargin, 310, newLeftMargin + labelWidth, 340); // Updated position
+    DrawText(hdc, L"Password:", -1, &passwordLabelRect, DT_SINGLELINE | DT_LEFT | DT_VCENTER | DT_NOCLIP | DT_NOPREFIX);
+
+    // Draw the password input field
+    RECT passwordRect;
+    SetRect(&passwordRect, newLeftMargin + labelWidth, 310, newRightMargin, 340); // Updated position
+    HBRUSH hPasswordBrush = CreateSolidBrush(RGB(255, 255, 255)); // White background color
+    FillRect(hdc, &passwordRect, hPasswordBrush);
+    DrawText(hdc, L"", -1, &passwordRect, DT_SINGLELINE | DT_LEFT | DT_VCENTER | DT_EDITCONTROL | DT_NOCLIP | DT_NOPREFIX);
+
+    // Cleanup
+    DeleteObject(hInputBrush);
+    DeleteObject(hPasswordBrush);
+    DeleteObject(hLabelFont);
+    DeleteObject(hInputFont);
+
+    // Draw the login button
+    RECT buttonRect;
+    SetRect(&buttonRect, width / 2 - 80, 380, width / 2 + 80, 420);
+
+    // Draw button background
+    HBRUSH hButtonBrush = CreateSolidBrush(RGB(79, 153, 202)); // Button color: #4f99ca
+    FillRect(hdc, &buttonRect, hButtonBrush);
+
+    // Draw button border
+    HPEN hBorderPen = CreatePen(PS_SOLID, 2, RGB(79, 153, 202)); // Border color: #b1cff1
+    SelectObject(hdc, hBorderPen);
+    Rectangle(hdc, buttonRect.left, buttonRect.top, buttonRect.right, buttonRect.bottom);
+
+    // Draw the button label
+    HFONT hButtonFont = CreateFont(24, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE, DEFAULT_CHARSET,
+        OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, L"Arial");
+    SelectObject(hdc, hButtonFont);
+
+    // Set the background mode to transparent
+    SetBkMode(hdc, TRANSPARENT);
+
+    // Calculate the center position
+    int textHeight = buttonRect.bottom - buttonRect.top;
+    SIZE textSize;
+    GetTextExtentPoint32(hdc, L"Login", 5, &textSize); // Measure the text size
+    int textX = buttonRect.left + (buttonRect.right - buttonRect.left - textSize.cx) / 2;
+    int textY = buttonRect.top + (buttonRect.bottom - buttonRect.top - textSize.cy) / 2;
+
+    // Draw the centered text label
+    TextOut(hdc, textX, textY, L"Login", 5);
+
     EndPaint(hWnd, &ps);
 }
 
